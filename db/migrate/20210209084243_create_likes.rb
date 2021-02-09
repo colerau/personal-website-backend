bed5f75b4f04f50a32eba462c7ee8b9c87d0f6ad
Cole Rau <colevrau@gmail.com>
2021-02-09 01:46:35 -0800

    saved two files and verified everything still working

--- db/migrate/20210209084243_create_likes.rb
+++ db/migrate/20210209084243_create_likes.rb
@@ -1,7 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
-      t.integer :count
 
       t.timestamps
     end
--- db/migrate/20210209084400_create_dislikes.rb
+++ db/migrate/20210209084400_create_dislikes.rb
@@ -1,7 +1,6 @@
 class CreateDislikes < ActiveRecord::Migration[6.0]
   def change
     create_table :dislikes do |t|
-      t.integer :count
 
       t.timestamps
     end
